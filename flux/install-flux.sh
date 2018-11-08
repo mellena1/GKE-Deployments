93b090b66100434c2f30303d6bc98796c32f3e81
mellena1 <andrew.mellen@monotype.com>
2018-11-08 07:56:03 -0500

    Move flux vals into a file and add resource limits

--- flux/install-flux.sh
+++ flux/install-flux.sh
@@ -5,27 +5,11 @@ helm repo add weaveworks https://weaveworks.github.io/flux
 git clone https://github.com/weaveworks/flux.git
 
 if ! helm status flux &> /dev/null; then
-    helm install --name flux \
-        --set helmOperator.create=true \
-        --set image.repository="quay.io/squaremo/flux" \
-        --set image.tag="5a7b7fe" \
-        --set helmOperator.repository="quay.io/squaremo/helm-operator" \
-        --set helmOperator.tag="5a7b7fe" \
-        --set git.url=ssh://git@github.com/mellena1/GKE-Deployments \
-        --set git.branch=master \
-        --set git.path=prod \
+    helm install --name flux -f values.yaml \
         --namespace flux \
         ./flux/chart/flux
 else
-    helm upgrade flux \
-        --set helmOperator.create=true \
-        --set image.repository="quay.io/squaremo/flux" \
-        --set image.tag="5a7b7fe" \
-        --set helmOperator.repository="quay.io/squaremo/helm-operator" \
-        --set helmOperator.tag="5a7b7fe" \
-        --set git.url=ssh://git@github.com/mellena1/GKE-Deployments \
-        --set git.branch=master \
-        --set git.path=prod \
+    helm upgrade flux -f values.yaml \
         --namespace flux \
         ./flux/chart/flux
 fi
